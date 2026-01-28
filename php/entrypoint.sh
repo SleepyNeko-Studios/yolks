35ea33a6ec2d0bc43d8030d6a135e3bd3541c327
watermelon <76525576+watermelon1024@users.noreply.github.com>
2026-01-28 14:57:31 +0800

    feat: Add PHP Docker build workflow and image
    Introduces a GitHub Actions workflow for building and pushing PHP Docker images with multiple version tags. Adds a Dockerfile based on dunglas/frankenphp, installs required PHP extensions, and includes a custom entrypoint script for container startup.

---
+++ php/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+
+cd /home/container
+
+# Print current PHP version
+php -v
+
+# 2. Handle the startup command passed from Pterodactyl
+# This line replaces the {{variables}} in the panel with actual environment variables
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# 3. Execute the command
+eval ${MODIFIED_STARTUP}
