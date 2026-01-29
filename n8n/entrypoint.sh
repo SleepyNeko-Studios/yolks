5a5496243dd1d6912670397b5849bda88dba6da6
watermelon <76525576+watermelon1024@users.noreply.github.com>
2026-01-29 20:00:49 +0800

    feat: add workflow and Docker for n8n
    Introduces a GitHub Actions workflow to build and push n8n Docker images for multiple tags. Adds a custom Dockerfile and entrypoint script to support custom CA certificates and Pterodactyl startup commands.

---
+++ n8n/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /home/container
+
+# Print current Node.js version
+node -v
+
+# 2. Handle the startup command passed from Pterodactyl
+# This line replaces the {{variables}} in the panel with actual environment variables
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# 3. Execute the command
+eval "exec ${MODIFIED_STARTUP}"
